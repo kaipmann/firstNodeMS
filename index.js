24c42f76ddac5158b11e1fd600f218b7c782a5de
kaipmann <tharun.mannaparambil@gmail.com>
2024-11-30 22:05:19 -0500

    Final

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const express = require('express');
 const app = express();
 
 //port :3001
-const port = 3001;
+const port = process.env.VCAP_APP_PORT || 3001;
 
 app.use(express.json());
 
 //root route or root path
 app.get('/', (req,res) => {
-    res.send("Hello team");
+    res.send("Hello team Sastrageek BTP Developers");
 });
 
 // route person
-//res.json({})
+app.get('/person', (req,res) => {
+    res.json({"personTab":[ {"firstName":"Ron","age":25},{"firstName":"Sam","age":35},{"firstName":"Tom","age":20}]});
+});
+// res.json({})
 
 // route weather
+app.get('/weather', (req, res) => {var request = require('request');
+    var options = {
+      'method': 'GET',
+      'url': 'https://api.open-meteo.com/v1/forecast?latitude=28.6519&longitude=77.2315&hourly=temperature_2m&forecast_days=1',
+      'headers': {
+      }
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
+    });
 
 
 //route /user
